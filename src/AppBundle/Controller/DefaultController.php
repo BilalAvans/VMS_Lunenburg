522326a2a839b0d6561511e00c47f45b429b8663
ThimoAvans <38351358+ThimoAvans@users.noreply.github.com>
2018-05-17 11:21:04 +0200

    test9 ongedaan

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -19,6 +19,4 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
-
-    // hallo
 }
